0f5ea5e504b70db419824d3d832fc84e29e7c7ca
Rohit Mahto <rohitmahto267@gmail.com>
2020-06-18 13:00:33 +0530

    Added SplashActivity

---
+++ app/src/main/java/com/shareindia/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.shareindia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {  // Changes activity after 2 seconds
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+
+
+    }
+}
